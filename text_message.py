c50c4586f59a1c5704edc1fe32946680c79eabb7
edifius <43631459+edifius@users.noreply.github.com>
2019-01-31 09:59:08 -0800

    Update text_message.py

--- text_message.py
+++ text_message.py
@@ -1,8 +1,6 @@
 import os
 from twilio.rest import Client
 
-account_sid = "AC8dd7c3947acb406b1dddd6a4bfeedc89"
-auth_token = "2BF75A7799C79BCA152D423526132D14"
 
 client = Client(account_sid, auth_token)
 
